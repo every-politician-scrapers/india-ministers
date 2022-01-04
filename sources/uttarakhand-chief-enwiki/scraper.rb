c25189338e036c125d06ebaec6379ea3e31d2999
Tony Bowden <tony@tmtm.com>
2022-01-04 17:32:42 +0200

    compare enwiki Uttarakhand Chief Ministers

---
+++ sources/uttarakhand-chief-enwiki/scraper.rb
@@ -0,0 +1,30 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+require 'open-uri/cached'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Portrait'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[no image name constituency start end duration assembly party].freeze
+    end
+
+    def empty?
+      itemLabel.include? "President's rule"
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
