62ffdbee37fe901870b6dce539a61b2ad1f899b4
Tony Bowden <tony@tmtm.com>
2022-01-04 22:03:53 +0200

    compare enwiki Chief of Staff

---
+++ sources/chief-of-staff-enwiki/scraper.rb
@@ -0,0 +1,43 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+require 'open-uri/cached'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Portrait'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[no image name start end duration branch ref].freeze
+    end
+
+    def empty?
+      itemLabel.to_s.empty? || itemLabel.include?("Vacant")
+    end
+
+    # First part of name field is the rank
+    def item
+      name_cell.css('a/@wikidata').map(&:text)[1]
+    end
+
+    def itemLabel
+      name_cell.css('a').map(&:text)[1]
+    end
+
+    def raw_end
+      super.gsub('†', '')
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
