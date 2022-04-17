6c0cc1718d98b30ff89b932a25869997be419325
Tony Bowden <tony@tmtm.com>
2022-04-17 08:42:15 +0300

    less verbose comparisons

--- bin/compare-chief-ministers.sh
+++ bin/compare-chief-ministers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-bundle exec ruby bin/scraper/chief-ministers-wikipedia.rb | ifne tee data/chief-ministers-wikipedia.csv
-bundle exec ruby bin/diff-chief-ministers.rb | ifne tee data/diff-chief-ministers.csv
+bundle exec ruby bin/scraper/chief-ministers-wikipedia.rb > data/chief-ministers-wikipedia.csv
+bundle exec ruby bin/diff-chief-ministers.rb > data/diff-chief-ministers.csv
